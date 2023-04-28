c63e69827b60484e672395c350a067d39946cd9d
Kamary <gillespiekamary@gmail.com>
2023-04-27 21:25:43 -0600

    heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
